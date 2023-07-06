a6cf4ef3b685a592225b470654412d729c697d48
furkanerdogan1 <furkanerdogannnn@gmail.com>
2023-07-06 20:19:19 +0300

    Automapper has been implemented

---
+++ bsStoreApp/Entities/DataTransferObjects/BookDtoForUpdate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DataTransferObjects
+{
+    public record BookDtoForUpdate(int Id, String Title, decimal Price);
+
+    //public class BookDtoForUpdate
+    //{
+    //    public int Id { get; init; }
+    //    public String Title { get; init; }
+    //    public decimal Price { get; init; }
+    //}
+}
